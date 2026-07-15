08d71e07e28545cc95c4e1f74987922891a2b9ac
caizhongqi <49096944+caizhongqi@users.noreply.github.com>
2026-07-15 15:52:08 +0800

    Export time-series inversion regularizers

--- src/qrecon/attacks/__init__.py
+++ src/qrecon/attacks/__init__.py
@@ -1,10 +1,24 @@
 from .analytic_linear import infer_class_label_from_last_bias, invert_first_linear_gradient
-from .gradient_inversion import AttackResult, GradientInversionAttack, leak_gradients
+from .gradient_inversion import (
+    AttackResult,
+    GradientInversionAttack,
+    gradient_matching_loss,
+    leak_gradients,
+)
+from .time_series_regularization import (
+    linear_trend_penalty,
+    periodicity_penalty,
+    resolution_consistency_penalty,
+)
 
 __all__ = [
     "AttackResult",
     "GradientInversionAttack",
+    "gradient_matching_loss",
     "leak_gradients",
     "invert_first_linear_gradient",
     "infer_class_label_from_last_bias",
+    "linear_trend_penalty",
+    "periodicity_penalty",
+    "resolution_consistency_penalty",
 ]
