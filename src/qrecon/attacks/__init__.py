572726cf45a301e2bf687186c36e0136006968cd
caizhongqi <49096944+caizhongqi@users.noreply.github.com>
2026-07-15 16:01:01 +0800

    Export analytic final-head representation leakage baseline

--- src/qrecon/attacks/__init__.py
+++ src/qrecon/attacks/__init__.py
@@ -5,6 +5,14 @@ from .gradient_inversion import (
     gradient_matching_loss,
     leak_gradients,
 )
+from .head_representation import (
+    HeadRepresentationAttackResult,
+    HeadRepresentationInversionAttack,
+    LinearHeadLeakageReport,
+    capture_final_linear_input,
+    find_last_biased_linear,
+    recover_single_effective_head_input,
+)
 from .time_series_regularization import (
     linear_trend_penalty,
     periodicity_penalty,
@@ -14,8 +22,14 @@ from .time_series_regularization import (
 __all__ = [
     "AttackResult",
     "GradientInversionAttack",
+    "HeadRepresentationAttackResult",
+    "HeadRepresentationInversionAttack",
+    "LinearHeadLeakageReport",
+    "capture_final_linear_input",
+    "find_last_biased_linear",
     "gradient_matching_loss",
     "leak_gradients",
+    "recover_single_effective_head_input",
     "invert_first_linear_gradient",
     "infer_class_label_from_last_bias",
     "linear_trend_penalty",
