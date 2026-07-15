dbab79e807636463c8607fa0534ab45ec5bb0817
caizhongqi <49096944+caizhongqi@users.noreply.github.com>
2026-07-15 16:19:13 +0800

    Export gradient-release and defense-aware inversion APIs

--- src/qrecon/attacks/__init__.py
+++ src/qrecon/attacks/__init__.py
@@ -5,6 +5,15 @@ from .gradient_inversion import (
     gradient_matching_loss,
     leak_gradients,
 )
+from .gradient_release import (
+    GradientRelease,
+    GradientReleaseSpec,
+    clip_gradient_tuple,
+    gradient_tuple_l2_norm,
+    last_biased_linear_parameter_indices,
+    quantize_gradient_tuple,
+    release_gradients,
+)
 from .head_representation import (
     HeadRepresentationAttackResult,
     HeadRepresentationInversionAttack,
@@ -12,6 +21,10 @@ from .head_representation import (
     capture_final_linear_input,
     find_last_biased_linear,
     recover_single_effective_head_input,
+)
+from .released_gradient_inversion import (
+    CandidateReleaseTransformReport,
+    ReleasedGradientInversionAttack,
 )
 from .time_series_regularization import (
     linear_trend_penalty,
@@ -21,18 +34,27 @@ from .time_series_regularization import (
 
 __all__ = [
     "AttackResult",
+    "CandidateReleaseTransformReport",
     "GradientInversionAttack",
+    "GradientRelease",
+    "GradientReleaseSpec",
     "HeadRepresentationAttackResult",
     "HeadRepresentationInversionAttack",
     "LinearHeadLeakageReport",
+    "ReleasedGradientInversionAttack",
     "capture_final_linear_input",
+    "clip_gradient_tuple",
     "find_last_biased_linear",
     "gradient_matching_loss",
-    "leak_gradients",
-    "recover_single_effective_head_input",
-    "invert_first_linear_gradient",
+    "gradient_tuple_l2_norm",
     "infer_class_label_from_last_bias",
+    "invert_first_linear_gradient",
+    "last_biased_linear_parameter_indices",
+    "leak_gradients",
     "linear_trend_penalty",
     "periodicity_penalty",
+    "quantize_gradient_tuple",
+    "recover_single_effective_head_input",
+    "release_gradients",
     "resolution_consistency_penalty",
 ]
