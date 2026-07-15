e57c0ac79c4f88a642b4f3edf125285fe3e3570c
caizhongqi <49096944+caizhongqi@users.noreply.github.com>
2026-07-15 14:56:07 +0800

    Export modern forecasting victims

--- src/qrecon/models/__init__.py
+++ src/qrecon/models/__init__.py
@@ -1,4 +1,26 @@
+from .factory import FORECASTING_ARCHITECTURES, build_forecasting_model
 from .images import ImageMLP, SmallLeNet, TinyConvNet
+from .modern_time_series import (
+    ITransformer,
+    PatchTST,
+    PatchTSTGeometry,
+    RevIN,
+    TransformerForecaster,
+    patchtst_geometry,
+)
 from .time_series import ForecastMLP
 
-__all__ = ["ImageMLP", "SmallLeNet", "TinyConvNet", "ForecastMLP"]
+__all__ = [
+    "FORECASTING_ARCHITECTURES",
+    "ForecastMLP",
+    "ITransformer",
+    "ImageMLP",
+    "PatchTST",
+    "PatchTSTGeometry",
+    "RevIN",
+    "SmallLeNet",
+    "TinyConvNet",
+    "TransformerForecaster",
+    "build_forecasting_model",
+    "patchtst_geometry",
+]
